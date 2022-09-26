aac41424c7797a4279f39da6f79aea32381b8989
rilex001 <ristic1900@gmail.com>
2022-09-26 17:50:09 +0200

    initial

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+const About = () => {
+    return ( 
+        <>
+        <Head>
+            <title>Ninja List | About</title>
+            <meta name='keywords' content='ninjas' />
+        </Head>
+        <div>
+            <h1>About</h1>
+            <p>Loremfodsnkcjdsnsdsd</p>
+        </div>
+        </>
+     );
+}
+ 
+export default About;
